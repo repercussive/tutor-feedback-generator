4c7e74762a3ab41df72cda878b455f099cde2911
Liam Robertson <liam2robertson@gmail.com>
2024-12-05 16:50:26 +0000

    Save additional comments to local storage

--- src/components/AdditionalCommentSection.tsx
+++ src/components/AdditionalCommentSection.tsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react'
 import { useFeedbackStore } from '@src/stores/feedbackStore'
+import { useDebouncedCallback } from 'use-debounce'
 import styles from '@components/styles/AdditionalCommentsSection.module.scss'
+
+const storedComments = localStorage.getItem('additionalComments')
+let hasLoadedCommentsFromLocalStorage = false
 
 function AdditionalCommentsSection() {
   const additionalComments = useFeedbackStore((state) => state.additionalComments)
   const setAdditionalComments = useFeedbackStore((state) => state.setAdditionalComments)
+
+  const saveAdditionalCommentsToLocalStorage = useDebouncedCallback((comments: string) => {
+    localStorage.setItem('additionalComments', comments)
+  }, 1000, { maxWait: 5000 })
+
+  const handleChangeComments = (comments: string) => {
+    setAdditionalComments(comments)
+    saveAdditionalCommentsToLocalStorage(comments)
+  }
+
+  useEffect(() => {
+    if (!hasLoadedCommentsFromLocalStorage) {
+      setAdditionalComments(storedComments ?? '')
+    }
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -16,7 +36,7 @@ function AdditionalCommentsSection() {
       <textarea
         id="additional-comments-input"
         value={additionalComments}
-        onChange={(e) => setAdditionalComments(e.target.value)}
+        onChange={(e) => handleChangeComments(e.target.value)}
         rows={6}
       />
     </div>
